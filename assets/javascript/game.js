462c71e4828d508d0c03565b16ad0108b3137f5e
Youngchan Chi <YCCHI@YCs-MacBook-Pro.local>
2019-03-28 16:30:11 -0400

    added newGame() function to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,23 +9,36 @@ var winCounter = 0;
 var loseCounter = 0;
 
 // Number of guesses left
-var guessesLeft = 9;
+var guessesLeft = 10;
 
 // Empty array to store incorrect guesses
 var incorrectArr = [];
 
+var computerGuess = "";
+console.log(computerGuess)
 
+// Create a function to start/reset the game
+function newGame() {
+  //reset important game variables
+    // assign computerGuess a new random letter
+    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)]
+    console.log("COMP GUESS: " + computerGuess)
+    // reset the incorrectArr
+    incorrectArr = [];
+    
+    // reset guessesLeft back to 10
+    guessesLeft = 10;
+
+    // write some information to the page indicating a new game has started
+    alert("NEW GAME!")
+}
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
-
-  // randomly select a letter from alphabet array.
-  var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)]
-  console.log(computerGuess)
-
+  
   // store the user pressed key in a variable
   var userGuess = event.key;
-  console.log(userGuess);
+  console.log("UserInput: " + userGuess);
   
     // Check if the user pressed key matches the letter.
     if (userGuess === computerGuess) {
@@ -35,6 +48,10 @@ document.onkeyup = function (event) {
       guessesLeft = 9;
       // reset incorrectArr to an empty array;
       incorrectArr = [];
+      //alert user
+      alert("You Won!")
+      //start a new game newGame()
+      newGame();
 
     } else {
       // if values does not match, decrement guessesCounter by one.
@@ -46,9 +63,9 @@ document.onkeyup = function (event) {
 
     // when guessesLeft becomes 0, resets guessesLeft to 9, and increment loseCounter by 1, and resets incorrectArr to empty array
     if (guessesLeft === 0) {
-      guessesLeft = 9;
+      alert("You Lost!")
       loseCounter++;
-      incorrectArr = [];
+      newGame();
     }
   
   //console.log to debug
@@ -57,16 +74,21 @@ document.onkeyup = function (event) {
   console.log("Guesses left: " + guessesLeft);
 
   //DOM manipulation
-  var winHTML = document.getElementById("wins");
-  winHTML.textContent = winCounter;
+var winHTML = document.getElementById("wins");
+winHTML.textContent = winCounter;
 
-  var loseHTML = document.getElementById("loses");
-  loseHTML.textContent = loseCounter;
+var loseHTML = document.getElementById("loses");
+loseHTML.textContent = loseCounter;
 
-  var guessHTML = document.getElementById("guesses");
-  guessHTML.textContent = guessesLeft;
+var guessHTML = document.getElementById("guesses");
+guessHTML.textContent = guessesLeft;
 
-  var incorrectHTML = document.getElementById("incorrect");
-  incorrectHTML.textContent = incorrectArr
+var incorrectHTML = document.getElementById("incorrect");
+incorrectHTML.textContent = incorrectArr
 
 }
+
+
+
+
+newGame();
